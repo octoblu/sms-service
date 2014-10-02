988ff19301a1fa7230af41b72d73fc80fb82ffb8
kmariano <koshin.mariano@gmail.com>
2014-10-01 17:03:10 -0700

    Using request to send messages

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var smsConfig = {
   plivo : {
     authId : process.env.PLIVO_AUTH_UUID,
     authToken : process.env.PLIVO_AUTH_TOKEN,
-    srcNumber : '14805001499'
+    srcNumber : '18583650671'
   },
   meshblu : {
     server : process.env.MESHBLU_SERVER || 'https://meshblu.octoblu.com',
--- controllers/send-sms-controller.js
+++ controllers/send-sms-controller.js
@@ -3,33 +3,45 @@ var mime = require('rest/interceptor/mime');
 var errorCode = require('rest/interceptor/errorCode');
 var basicAuth = require('rest/interceptor/basicAuth');
 var config = require('../config/config');
-var client = rest.wrap(mime).wrap(errorCode).wrap(basicAuth, {username : config.plivo.authId, password : config.plivo.authToken});
+var client = rest.wrap(mime).wrap(errorCode).wrap(basicAuth, {username: config.plivo.authId, password: config.plivo.authToken});
+var request = require('request');
 
-var SendSMSController = function(){
+var SendSMSController = function () {
   var self = this;
-  self.sendSMSMessage = function(req, res){
-    client({
+  self.sendSMSMessage = function (req, res) {
+    request({
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'Octoblu',
+        'x-li-format': 'json'
+      },
+      json : {src : config.plivo.srcNumber, dst : req.body.dst, text : req.body.text},
+      uri: 'https://' + config.plivo.authId + ':' + config.plivo.authToken + '@api.plivo.com/v1/Account/' + config.plivo.authId + '/Message/',
       method: 'POST',
-      path: 'https://api.plivo.com/v1/Account/' + config.plivo.authId + '/Message/',
-      entity : {
-        src : config.plivo.srcNumber,
-        dst : req.body.dst,
-        text : req.body.text
+      followAllRedirects: true
+
+    }, function (error, response, body) {
+      var parsedBody;
+      if (error) {
+        console.log(error);
+        res.send(500, error);
       }
-    }).then(function (result) {
-      res.send(result.entity);
-    })
-    .catch(function (errorResult) {
-        res.send(errorResult.status.code, 'Error Sending SMS Message!');
+
+      try {
+        parsedBody = JSON.parse(body);
+        res.send(parsedBody);
+      } catch (err) {
+        res.send(200, body);
+      }
     });
   };
-  self.getSMSMessage = function(req, res, next){
+  self.getSMSMessage = function (req, res, next) {
     client({
       method: 'POST',
-      path: 'https://api.plivo.com/v1/Account/' + config.plivo.authId + '/Message/' + req.params.id,
-      params : {
-        uuid : uuid,
-        token : token
+      path: 'https://' + config.plivo.authId + ':' + config.plivo.authToken + '@api.plivo.com/v1/Account/' + config.plivo.authId + '/Message/' + req.params.id,
+      params: {
+        uuid: uuid,
+        token: token
       }
     }).then(function (result) {
       res.send(result.entity);
