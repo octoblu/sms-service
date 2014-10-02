4d38de648a97d0d54d0b401bcaa249dea4ae3e8f
Jade Meskill <jade.meskill@gmail.com>
2014-10-01 22:08:16 -0700

    clean up, add public

--- server.js
+++ server.js
@@ -8,47 +8,24 @@ var express = require('express'),
   session = require('express-session'),
   bodyParser = require('body-parser'),
   errorHandler = require('errorhandler');
-var port = process.env.SMS_PORT || process.env.PORT || 9009;
 
 app = express();
+app.set('port', process.env.SMS_PORT || process.env.PORT || 9009);
 app.use(bodyParser.json());
 app.use(session({resave: true, saveUninitialized: true, secret: 'sqrt0fSaturn'}));
 app.use(morgan('combined'));
 app.use(methodOverride());
-app.use(errorHandler());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler());
 
 var meshbluAuth = require('./middleware/meshblu-auth');
 
 var SendSMSController = require('./controllers/send-sms-controller');
 var sendSMSController = new SendSMSController(config);
 
-
 app.post('/message', meshbluAuth.authenticate,  sendSMSController.sendSMSMessage);
 app.get('/message/:id', meshbluAuth.authenticate,  sendSMSController.getSMSMessage);
 
-var httpServer = http.createServer(app);
-
-httpServer.listen(port, function () {
-  console.log('Listening on port ' + port);
+app.listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
