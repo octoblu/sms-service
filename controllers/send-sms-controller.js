56d594f5a1fcd11df196bcfac61e2deff853bee0
Sqrt of Saturn <sqrt@octoblu.com>
2015-11-25 11:03:58 -0700

    v1.0.7 remove crash if text is not defined

--- controllers/send-sms-controller.js
+++ controllers/send-sms-controller.js
@@ -1,9 +1,23 @@
 var config = require('../config/config');
 var request = require('request');
+var _ = require('lodash');
 
 var SendSMSController = function () {
   var self = this;
+
+  self.sendResponse = function (res, code) {
+    return function (error, response, body) {
+      if (error) {
+        return res.status(code).send(error.message);
+      }
+      res.json(body);
+    }
+  };
+
   self.sendSMSMessage = function (req, res) {
+    if ( !(req && req.body && _.isString(req.body.text)) ) {
+      return self.sendResponse(res,422)(new Error('body text field is not a string'));
+    }
 
     request({
       headers: {
@@ -14,13 +28,7 @@ var SendSMSController = function () {
       uri: 'https://' + config.plivo.authId + ':' + config.plivo.authToken + '@api.plivo.com/v1/Account/' + config.plivo.authId + '/Message/',
       method: 'POST',
       followAllRedirects: true
-
-    }, function (error, response, body) {
-      if (error) {
-        return res.status(422).send(error.message);
-      }
-      res.json(body);
-    });
+    }, self.sendResponse(res,422));
   };
 
   self.getSMSMessage = function (req, res, next) {
@@ -33,12 +41,7 @@ var SendSMSController = function () {
       uri: 'https://' + config.plivo.authId + ':' + config.plivo.authToken + '@api.plivo.com/v1/Account/' + config.plivo.authId + '/Message/' + req.params.id,
       followAllRedirects: true
 
-    }, function (error, response, body) {
-      if (error) {
-        return res.send(500);
-      }
-      res.json(body);
-    });
+    }, self.sendResponse(res,500));
   };
   return self;
 };
