d0775c3d58364968b98b51ea4fe9a1304c2ea1bd
Jade Meskill <jade.meskill@gmail.com>
2016-02-17 16:09:37 -0700

    v1.0.9 use production dockerfile, handle sigterm, move healthcheck

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
-FROM node:0.10-onbuild
-MAINTAINER Octoblu <docker@octoblu.com>
+FROM node:5
+MAINTAINER Octoblu, Inc. <docker@octoblu.com>
+
 EXPOSE 80
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app/
+RUN npm -s install --production
+COPY . /usr/src/app/
+
+CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   errorHandler = require('errorhandler');
 
+var meshbluHealthcheck = require('express-meshblu-healthcheck');
+
 app = express();
 app.set('port', process.env.SMS_PORT || process.env.PORT || 80);
+app.use(meshbluHealthcheck());
 app.use(bodyParser.json());
 app.use(morgan('dev', { skip: function(req, res){ return res.statusCode < 400 }}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(errorHandler());
-
-var meshbluHealthcheck = require('express-meshblu-healthcheck');
-app.use(meshbluHealthcheck());
 
 var meshbluAuth = require('express-meshblu-auth');
 app.use(meshbluAuth({
@@ -33,4 +33,9 @@ app.get('/message/:id',  sendSMSController.getSMSMessage);
 
 app.listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
+});
+
+process.on('SIGTERM', function() {
+  console.log('SIGTERM caught, exiting');
+  process.exit(0);
 });
